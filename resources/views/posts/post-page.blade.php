674dcb15c6daf0a42375dc4f8ff03f0a61823b14
DanceWithTheDead <zhora.mosa@mail.ru>
2025-05-08 19:05:39 +0300

    add button for admin user

--- resources/views/posts/post-page.blade.php
+++ resources/views/posts/post-page.blade.php
@@ -5,7 +5,7 @@
             <div class="card mt-2">
                 <h5 class="card-header text-bg-primary font-bold">{{ $post->title }}</h5>
                 <div class="card-body">
-                    <h1 class="card-title">Special title treatment</h1>
+                    <h1 class="card-title">tags</h1>
                     <p class="card-text">{{ $post->content }}</p>
                     <a href="{{ route('post.show', $post->id) }}" class="btn btn-dark mt-2">Open post</a>
                 </div>
