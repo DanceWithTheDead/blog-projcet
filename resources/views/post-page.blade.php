d9697898e3cd341607e8341ff09a906501ef3e1b
DanceWithTheDead <zhora.mosa@mail.ru>
2025-05-08 14:00:42 +0300

    develop adequate display of posts has been made in blades

--- resources/views/post-page.blade.php
+++ resources/views/post-page.blade.php
@@ -2,10 +2,13 @@
     Post-page
     <x-slot:abb>
         @foreach( $posts as $post)
-            <div>
-                <h2>{{ $post->title }}</h2>
-                <p>{{ $post->content }}</p>
+        <div class="card mt-2" style="width: 50rem;">
+            <div class="card-body">
+                <h5 class="card-title">{{ $post->title }}</h5>
+                <p class="card-text">{{ $post->content }}</p>
+                <a href="/post" class="btn btn-primary">{{ $post->title }}</a>
             </div>
+        </div>
         @endforeach
     </x-slot:abb>
 </x-layout>
